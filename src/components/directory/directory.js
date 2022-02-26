119c956b7edd8c01f62bfc47bc3ad47e96fc42a9
Jagroop Singh <jagroopofficial2000@gmail.com>
2022-02-26 15:19:42 +0530

    Adding Functionality of Routes

--- src/components/directory/directory.js
+++ src/components/directory/directory.js
@@ -12,33 +12,33 @@ class Directory extends React.Component {
                   title: 'hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'hats'
                 },
                 {
                   title: 'jackets',
                   imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                   id: 2,
-                  linkUrl: 'shop/jackets'
+                  linkUrl: ''
                 },
                 {
                   title: 'sneakers',
                   imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                   id: 3,
-                  linkUrl: 'shop/sneakers'
+                  linkUrl: ''
                 },
                 {
                   title: 'womens',
                   imageUrl: 'https://www.iwmbuzz.com/wp-content/uploads/2021/11/5-times-blackpink-lisa-was-an-absolute-visual-queen-in-the-prettiest-gowns-920x518.jpg',
                   size: 'large',
                   id: 4,
-                  linkUrl: 'shop/womens'
+                  linkUrl: ''
                 },
                 {
                   title: 'mens',
                   imageUrl: 'https://static.toiimg.com/photo/msid-87242700/87242700.jpg?47260',
                   size: 'large',
                   id: 5,
-                  linkUrl: 'shop/mens'
+                  linkUrl: ''
                 }
               ]
         }
@@ -49,9 +49,9 @@ class Directory extends React.Component {
         return(
         <div className="directory-menu">
             {
-                this.state.sections.map(({title , imageUrl , id , size}) =>
+                this.state.sections.map(({ id, ...otherSectionProps }) =>
                 (
-                    <MenuItems  key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    <MenuItems  key={id} {...otherSectionProps}/>
                 ))
             }
 
--- src/components/menu-items/menu-item-component.js
+++ src/components/menu-items/menu-item-component.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import { withRouter } from "react-router-dom";
 import './menu-item-styles.scss'
 
-const MenuItems = ({title , imageUrl , size}) => (
-    <div className= {`${size} menu-item`}>
+const MenuItems = ({title , imageUrl , size, history, linkUrl}, match) => (
+    <div className= {`${size} menu-item`} onClick ={ () => {history.push(`${linkUrl}`)}}>
         <div 
         className="background-image"
         style={
@@ -20,4 +20,4 @@ const MenuItems = ({title , imageUrl , size}) => (
 </div>
 );
 
-export default MenuItems;
+export default withRouter(MenuItems);
